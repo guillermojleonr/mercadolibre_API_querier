5942541f27bc81c3d92de70a8739ef5f2a9f6903
RKE <>
2022-08-31 17:08:48 -0400

    Storing client data in-memory
    This was a failed approach to store client data in enviroment variables.
    
    There is a need to store this data in a persistent way.
    
    However the class method add_clients_info will still be there.

--- authentication/views.py
+++ authentication/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
+from app import app
+from django.conf import settings
+from .models import Client, Tokens
 # Create your views here.
 
 
-def login_view(request):    
+def login_view(request):
     return render(request, "authentication/login.html")
 
-def redirect_view(request):    
+def redirect_view(request):
+    get = request.GET
+    code = get.__getitem__('code')
+    app_instance = app()
+    tokens = app_instance.exchange_code_to_token(code)
+    user_id = tokens['user_id']
+    clients = app_instance.add_clients_info(user_id,tokens)
     return render(request, "authentication/redirect.html")
