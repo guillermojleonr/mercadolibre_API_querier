c4846766eb7295fda14ccc1a32947e3c10bcd5c7
guillermojleonr <88005383+guillermojleonr@users.noreply.github.com>
2022-09-20 02:05:58 -0300

    Improved authentication functionality To register data owners properly in the database

--- authentication/models.py
+++ authentication/models.py
@@ -1,11 +1,10 @@
-from tkinter import CASCADE
 from django.db import models
 
 # Create your models here.
 
 class Client(models.Model):
     name = models.CharField(max_length=50)
-    client_id = models.CharField(max_length=50)
+    client_id = models.CharField(max_length=50,unique=True)
     nickname = models.CharField(max_length=50)
     created=models.DateTimeField(auto_now_add=True)
     updated=models.DateTimeField(auto_now_add=True)
@@ -18,5 +17,8 @@ class Client(models.Model):
         return self.name
 
 class Tokens(models.Model):
-    client_id = models.ForeignKey(Client, on_delete=models.CASCADE)
-    refresh_token = refresh_token = models.CharField(max_length=50)
+    client_id = models.ForeignKey(Client, on_delete=models.CASCADE, to_field='client_id')
+    refresh_token = models.CharField(max_length=50)
+    access_token = models.CharField(max_length=50)
+    created=models.DateTimeField(auto_now_add=True)
+    updated=models.DateTimeField(auto_now_add=True)
--- authentication/views.py
+++ authentication/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from app import app
+from main.app import *
 from django.conf import settings
 from .models import Client, Tokens
 # Create your views here.
@@ -9,10 +9,36 @@ def login_view(request):
     return render(request, "authentication/login.html")
 
 def redirect_view(request):
+    # Object instances
+    app_instance = app()
+    client_instance = Client()
+    
     get = request.GET
     code = get.__getitem__('code')
-    app_instance = app()
-    tokens = app_instance.exchange_code_to_token(code)
+    tokens = app_instance.exchange_code_to_token(code) #Returns user id, access and refresh tokens in a dictionary
+    
+    #Data by field
     user_id = tokens['user_id']
-    clients = app_instance.add_clients_info(user_id,tokens)
+    access_token = tokens['access_token']
+    refresh_token = tokens['refresh_token']
+
+    #Gets the remaining client info
+    client_info = client_instance.get_client_info(user_id,access_token) #Returns dictionary
+
+    #Data by field
+    client_nickname = client_info['nickname']
+    client_name = client_info['first_name']
+
+    #Create new client or get it if already exist.
+    Client.objects.get_or_create(
+    client_id = user_id,
+    defaults={'name':client_name,'nickname':client_nickname},
+    )
+
+    #Create or update Tokens.
+    Tokens.objects.update_or_create(
+    client_id_id = user_id,
+    defaults={'client_id_id':user_id,'refresh_token':refresh_token,'access_token':access_token}
+    )
+    
     return render(request, "authentication/redirect.html")
