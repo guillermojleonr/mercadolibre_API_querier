1fe477461592dfb287acaf855715dc4fce5a9ee3
guillermojleonr <88005383+guillermojleonr@users.noreply.github.com>
2022-10-11 01:02:22 -0300

    static files fixed

--- ml_querier/settings.py
+++ ml_querier/settings.py
@@ -147,13 +147,12 @@ USE_TZ = True
 # If you don't have this directory and have this uncommented your build will fail
 # STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 
-# STATIC_URL = '/static/'
-STATIC_URL = 'static/'
+# STATIC_URL = '/static/' #absolutepath
+STATIC_URL = 'static/' #relative path
 
 # Following settings only make sense on production and may break development environments.
-if not DEBUG:    # Tell Django to copy statics to the `staticfiles` directory
-    # in your application directory on Render.
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+if not DEBUG:    # Tell Django to copy statics to the `static` directory in your application directory on Render.
+    STATIC_ROOT = os.path.join(BASE_DIR, 'main\static')
     # Turn on WhiteNoise storage backend that takes care of compressing static files
     # and creating unique names for each version so they can safely be cached forever.
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
