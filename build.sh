7b3758eb95abed7e2242ec3ae24f804164d2a805
guillermojleonr <88005383+guillermojleonr@users.noreply.github.com>
2022-10-11 02:32:33 -0300

    superuser settings

--- build.sh
+++ build.sh
@@ -5,4 +5,4 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
-django-admin createsuperuser --noinput
+python manage.py createsuperuser --noinput --username $DJANGO_SUPERUSER_USERNAME --email $DJANGO_SUPERUSER_EMAIL --password $DJANGO_SUPERUSER_PASSWORD
