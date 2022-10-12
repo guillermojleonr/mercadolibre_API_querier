c74c3bd27ae611905fee64a9c1cf2e5afeca1cb5
RKE <>
2022-10-12 12:15:06 -0300

    New command createsuperuser_if_none_exist
    To automate the createsuperuser process on build.

--- build.sh
+++ build.sh
@@ -5,4 +5,4 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
-python manage.py createsuperuser --noinput --username $DJANGO_SUPERUSER_USERNAME --email $DJANGO_SUPERUSER_EMAIL --password $DJANGO_SUPERUSER_PASSWORD
+python manage.py createsuperuser_if_none_exists --user=gleon --password=changeme --email=gleon@savingl.cl
